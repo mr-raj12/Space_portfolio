5ca6cd71588b5cea928b6927e81558ae2e44bcbd
Mrityunjay Raj <mr.raj.earth@gmail.com>
2024-10-15 08:50:44 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Add this line
-    // Other configurations can go here
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export', // Add this line
+  // Other configurations can go here
+};
+
+export default nextConfig; // Change this line
